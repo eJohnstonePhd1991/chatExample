d4c9cfb29b1f007f1776a3fdc115f0a82bb0c96b
Ewan Johnstone <e.johnstone1991@gmail.com>
2018-03-22 22:58:04 +0000

    fixed port allocation

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
+const Port = process.env.PORT || 3000;
 var users = [];
 app.get('/', (req,res)=> {
     res.sendFile(__dirname + '/index.html');
@@ -9,13 +10,13 @@ app.get('/', (req,res)=> {
 io.on('connection', (socket)=>{
 
     // sends a message to other users that a new user has joined
-    
-    
+
+
     //Event that sends new messages to everyone on the server
     socket.on('sent message', (data)=>{
         socket.broadcast.emit('recieve message', data);
     });
-    
+
     //new user logs in
     socket.on('newUsername', (data)=>{
         console.log("username is " + data);
@@ -25,7 +26,7 @@ io.on('connection', (socket)=>{
         //Notifies all other users
         console.log(users);
         socket.broadcast.emit('online', data);
-        
+
         //TODO send list of current users to new socket
         io.emit('updateUsers', users);
     })
@@ -35,12 +36,12 @@ io.on('connection', (socket)=>{
         //remove name from list
         var index = users.indexOf(socket.name);
         users.splice(index,1);
-        
+
         console.log(users);
         io.emit('updateUsers', users);
     })
 });
 
-http.listen(3000, ()=>{
-    console.log('listening on *:3000');
+http.listen(Port, ()=>{
+    console.log(`listening on *:` + Port);
 });
